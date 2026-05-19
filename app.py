9a8108eda98c65d376e75b2ceab879d23d6a8b0f
cyberHawk UG <mcgz0783@gmail.com>
2026-05-19 23:44:19 +0300

    Update route to serve ash_advisor.html

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route('/ash_agent')
 def ash_agent():
-    return send_from_directory('.', 'ash_advisor-1.html')
+    return send_from_directory('.', 'ash_advisor.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
